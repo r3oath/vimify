216c75abe48fa232745c8b23e0aefac394267d0b
Tristan Strathearn <tstrathearn@bcm.com.au>
2018-05-13 20:23:22 +1000

    Updates link script.

--- link.sh
+++ link.sh
@@ -1,3 +1,4 @@
 (ln -s ~/vimify/.bash_profile ~/.bash_profile)
 (ln -s ~/vimify/.tmux.conf ~/.tmux.conf)
 (ln -s ~/vimify/.vimrc ~/.vimrc)
+(mkdir -p ~/.config/nvim; ln -s ~/vimify/init.vim ~/.config/nvim/init.vim)
